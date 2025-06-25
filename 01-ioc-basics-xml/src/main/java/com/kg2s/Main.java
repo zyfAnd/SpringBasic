a9638f720c6f21f02d0258ef68ae575be7363716
zyfAnd <zhangyanfu66@gmail.com>
2025-06-25 15:02:39 +0800

    updated

--- 01-ioc-basics-xml/src/main/java/com/kg2s/Main.java
+++ 01-ioc-basics-xml/src/main/java/com/kg2s/Main.java
@@ -11,5 +11,7 @@ public class Main {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
         context.getBean("controller1",  GreetingController.class).sayHello("Zhang San");
+        context.getBean("controller2",  GreetingController.class).sayHello("Li Si");
+        context.getBean("controller3",GreetingController.class).sayHello("Wang Wu");
     }
 }
